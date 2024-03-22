3bdda3fddbf85224682552a110bcdf9abe6fbaee
Eliana Ostro <elianaostro@gmail.com>
2024-03-22 18:46:54 -0300

    h

--- ej1.py
+++ ej1.py
@@ -6,60 +6,77 @@ import matplotlib.pyplot as plt
 def f(x):
     return 0.3**abs(x) * np.sin(4*x) - np.tanh(2*x) +2
 
-def cubic_spline_interpolation(x, y):
-
-    interpolator = interp1d(x, y, kind='cubic')
-
+def lineal_spline_interpolation(x, y):
+    interpolator = interpolate.interp1d(x, y)
     return interpolator
 
-def lagranje(x):
-    n = len(x_splines)
+def quadratic_spline_interpolation(x, y):
+    interpolator = interpolate.interp1d(x, y, kind='quadratic')
+    return interpolator
+
+def cubic_spline_interpolation(x, y):
+    interpolator = interpolate.interp1d(x, y, kind='cubic')
+    return interpolator
+
+def lagranje15(x):
+    n = len(x_splines_15)
     result = 0.0
     for i in range(n):
-        term = f(x_splines[i])
+        term = f(x_splines_15[i])
         for j in range(n):
             if j != i:
-                term *= (x - x_splines[j]) / (x_splines[i] - x_splines[j])
+                term *= (x - x_splines_15[j]) / (x_splines_15[i] - x_splines_15[j])
         result += term
     return result
 
 # Generate x values
-n = 400
-x_values = np.linspace(-4, 4, n)
+x_values = np.linspace(-4, 4, 100)
 
-m = 15
-x_splines = np.linspace(-4, 4, m)
+x_splines_15 = np.linspace(-4, 4, 15)
+x_splines_25 = np.linspace(-4, 4, 25)
+x_splines_50 = np.linspace(-4, 4, 50)
+x_splines_75 = np.linspace(-4, 4, 75)
+x_splines_100 = np.linspace(-4, 4, 100)
 
 # Generate y values
 y_values = f(x_values)
+y_splines_15 = f(x_splines_15)
 
 # Plot the graph
-plt.plot(x_values, y_values, label='Ground Truth')
+plt.plot(x_values, y_values, label='Ground Truth', linestyle='--')
 
 spl = interpolate.UnivariateSpline(x_values, y_values)
+plt.plot(x_splines_15, spl(x_splines_15), label='Splines')
 
-plt.plot(x_splines, spl(x_splines), label='Splines')
+y_lagrange = lagranje15(x_splines_15)
+plt.plot(x_splines_15, y_lagrange,label='Lagrange')
 
-y_lagrange = lagranje(x_splines)
-plt.plot(x_splines, y_lagrange,label='Lagrange')
+y_lineal_spline = lineal_spline_interpolation(x_splines_15, y_splines_15)
+y_splines_lineal=[]
+for x in x_values:
+    y_splines_lineal.append(y_lineal_spline(x))
+plt.plot(x_values, y_splines_lineal, label='Lineal Spline')
 
-# for i in [100, 75, 50, 25, 15]:
-#     x_splines = np.linspace(-4, 4, i)
-#     plt.plot(x_splines, spl(x_splines), 'g')
+y_quadratic_spline = quadratic_spline_interpolation(x_splines_15, y_splines_15)
+y_splines_quadratic = []
+for x in x_values:
+    y_splines_quadratic.append(y_quadratic_spline(x))
+plt.plot(x_values, y_splines_quadratic, label='Quadratic Spline')
 
-#     # Generate y values for Lagrange interpolation
-#     y_lagrange = lagranje(x_splines)
-#     # Plot the Lagrange interpolation
-#     plt.plot(x_splines, y_lagrange)
+y_cubic_spline = cubic_spline_interpolation(x_splines_15, y_splines_15)
+y_splines_cubic = []
+for x in x_values:
+    y_splines_cubic.append(y_cubic_spline(x))
+plt.plot(x_values, y_splines_cubic, label='Cubic Spline')
 
 
 # Add labels and title
-plt.xlabel('x')
-plt.ylabel('f(x)')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
 #plt.title('Graph of $f(x) = x^2$')
 
 # Add grid
-plt.grid(True)
+plt.grid(False)
 
 # Add legend
 plt.legend()

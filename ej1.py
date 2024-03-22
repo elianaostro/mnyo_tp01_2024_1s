87b0cbfb3ba5451a2fa75c1fd71d6291b1891a3f
jojojab <jojojablon@gmail.com>
2024-03-22 16:21:00 -0300

    aaa

---
+++ ej1.py
@@ -0,0 +1,30 @@
+import nump as np
+import scypi as sp
+import matplotlib.pyplot as plt
+
+# Define the function
+def f(x):
+    return x ** 2
+
+# Generate x values
+x_values = np.linspace(-10, 10, 400)
+
+# Generate y values
+y_values = f(x_values)
+
+# Plot the graph
+plt.plot(x_values, y_values, label='$f(x) = x^2$')
+
+# Add labels and title
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Graph of $f(x) = x^2$')
+
+# Add grid
+plt.grid(True)
+
+# Add legend
+plt.legend()
+
+# Show the plot
+plt.show()

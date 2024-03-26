9ade1c20ed8d62b327084fc56206365c68ce8d15
jojojab <jojojablon@gmail.com>
2024-03-26 15:07:23 -0300

    bbbb

--- ej1.py
+++ ej1.py
@@ -32,76 +32,82 @@ def chebyshev_nodes(n, a=-4, b=4):
     nodes = 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) * np.pi / (2 * n))
     return np.sort(nodes)
 
-# Choose x-values and calculate corresponding y-values
-num_points = 10
-x_values = np.linspace(-4, 4, num_points)
-y_values = f(x_values)
 
-x_values_chebyshev = chebyshev_nodes(100)
-y_values_chebyshev = f(x_values_chebyshev)
+def main():
+    # Choose x-values and calculate corresponding y-values
+    num_points = 10
+    x_values = np.linspace(-4, 4, num_points)
+    y_values = f(x_values)
 
-x_values_chebyshev = chebyshev_nodes(num_points)
-y_values_chebyshev = f(x_values_chebyshev)
+    x_values_chebyshev = chebyshev_nodes(100)
+    y_values_chebyshev = f(x_values_chebyshev)
 
-x_ground_truth = np.linspace(-4, 4, 100)
-y_ground_truth = f(x_ground_truth)
+    x_values_chebyshev = chebyshev_nodes(num_points)
+    y_values_chebyshev = f(x_values_chebyshev)
 
-x_interpolated_lagrange = np.linspace(-4, 4, 100)
-y_interpolated_lagrange = lagrange_interpolation(x_values, y_values, x_interpolated_lagrange)
+    x_ground_truth = np.linspace(-4, 4, 100)
+    y_ground_truth = f(x_ground_truth)
 
-x_interpolated_lagrange_chebyshev = np.linspace(-4, 4, 100)
-y_interpolated_lagrange_chebyshev = lagrange_interpolation(x_values_chebyshev, y_values_chebyshev, x_interpolated_lagrange_chebyshev)
+    x_interpolated_lagrange = np.linspace(-4, 4, 100)
+    y_interpolated_lagrange = lagrange_interpolation(x_values, y_values, x_interpolated_lagrange)
 
-lagrange_error_values = lagrange_error(x_values, y_values, x_interpolated_lagrange)
-lagrange_error_values_chebyshev = lagrange_error(x_values_chebyshev, y_values_chebyshev, x_interpolated_lagrange_chebyshev)
+    x_interpolated_lagrange_chebyshev = np.linspace(-4, 4, 100)
+    y_interpolated_lagrange_chebyshev = lagrange_interpolation(x_values_chebyshev, y_values_chebyshev, x_interpolated_lagrange_chebyshev)
 
-x_interpolated_cubic = np.linspace(-4, 4, 100)
-y_interpolated_cubic = cubic_spline_interpolation(x_values, y_values, x_interpolated_cubic)
+    lagrange_error_values = lagrange_error(x_values, y_values, x_interpolated_lagrange)
+    lagrange_error_values_chebyshev = lagrange_error(x_values_chebyshev, y_values_chebyshev, x_interpolated_lagrange_chebyshev)
 
-x_interpolated_cubic_chebyshev = np.linspace(-4, 4, 100)
-y_interpolated_cubic_chebyshev = cubic_spline_interpolation(x_values_chebyshev, y_values_chebyshev, x_interpolated_cubic_chebyshev)
+    x_interpolated_cubic = np.linspace(-4, 4, 100)
+    y_interpolated_cubic = cubic_spline_interpolation(x_values, y_values, x_interpolated_cubic)
 
-cubic_spline_error_values = cubic_spline_error(x_values, y_values, x_interpolated_cubic)
-cubic_spline_error_values_chebyshev = cubic_spline_error(x_values_chebyshev, y_values_chebyshev, x_interpolated_cubic_chebyshev)
+    x_interpolated_cubic_chebyshev = np.linspace(-4, 4, 100)
+    y_interpolated_cubic_chebyshev = cubic_spline_interpolation(x_values_chebyshev, y_values_chebyshev, x_interpolated_cubic_chebyshev)
 
-fig, axs = plt.subplots(2, 2)
+    cubic_spline_error_values = cubic_spline_error(x_values, y_values, x_interpolated_cubic)
+    cubic_spline_error_values_chebyshev = cubic_spline_error(x_values_chebyshev, y_values_chebyshev, x_interpolated_cubic_chebyshev)
 
-axs[0,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
-axs[0,0].scatter(x_values, y_values, label='Interpolation Points', color='red', s=8)
-axs[0,0].plot(x_interpolated_lagrange, y_interpolated_lagrange, label='Lagrange Interpolation', color='green')
-axs[0,0].set_title('Lagrange Interpolation')
+    fig, axs = plt.subplots(2, 2)
 
-axs[0,1].plot(x_interpolated_lagrange, lagrange_error_values, label='Lagrange Error', color='red')
-axs[0,1].set_title('Lagrange Error')
+    axs[0,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
+    axs[0,0].scatter(x_values, y_values, label='Interpolation Points', color='red', s=8)
+    axs[0,0].plot(x_interpolated_lagrange, y_interpolated_lagrange, label='Lagrange Interpolation', color='green')
+    axs[0,0].set_title('Lagrange Interpolation')
 
-axs[1,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
-axs[1,0].scatter(x_values, y_values, label='Interpolation Points', color='red', s=8)
-axs[1,0].plot(x_interpolated_cubic, y_interpolated_cubic, label='Cubic Spline Interpolation', color='orange')
-axs[1,0].set_title('Cubic Spline Interpolation')
+    axs[0,1].plot(x_interpolated_lagrange, lagrange_error_values, label='Lagrange Error', color='red')
+    axs[0,1].set_title('Lagrange Error')
 
-axs[1,1].plot(x_interpolated_cubic, cubic_spline_error_values, label='Cubic Spline Error', color='red')
-axs[1,1].set_title('Cubic Spline Error')
+    axs[1,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
+    axs[1,0].scatter(x_values, y_values, label='Interpolation Points', color='red', s=8)
+    axs[1,0].plot(x_interpolated_cubic, y_interpolated_cubic, label='Cubic Spline Interpolation', color='orange')
+    axs[1,0].set_title('Cubic Spline Interpolation')
 
-plt.tight_layout()
-plt.show()
+    axs[1,1].plot(x_interpolated_cubic, cubic_spline_error_values, label='Cubic Spline Error', color='red')
+    axs[1,1].set_title('Cubic Spline Error')
 
-fig, axs = plt.subplots(2, 2)
+    plt.tight_layout()
+    plt.show()
 
-axs[0,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
-axs[0,0].scatter(x_values_chebyshev, y_values_chebyshev, label='Interpolation Points', color='red', s=8)
-axs[0,0].plot(x_interpolated_lagrange_chebyshev, y_interpolated_lagrange_chebyshev, label='Lagrange Interpolation', color='green')
-axs[0,0].set_title('Lagrange Interpolation (Chebyshev Nodes)')
+    fig, axs = plt.subplots(2, 2)
 
-axs[0,1].plot(x_interpolated_lagrange_chebyshev, lagrange_error_values_chebyshev, label='Lagrange Error', color='red')
-axs[0,1].set_title('Lagrange Error (Chebyshev Nodes)')
+    axs[0,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
+    axs[0,0].scatter(x_values_chebyshev, y_values_chebyshev, label='Interpolation Points', color='red', s=8)
+    axs[0,0].plot(x_interpolated_lagrange_chebyshev, y_interpolated_lagrange_chebyshev, label='Lagrange Interpolation', color='green')
+    axs[0,0].set_title('Lagrange Interpolation (Chebyshev Nodes)')
 
-axs[1,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
-axs[1,0].scatter(x_values_chebyshev, y_values_chebyshev, label='Interpolation Points', color='red', s=8)
-axs[1,0].plot(x_interpolated_cubic_chebyshev, y_interpolated_cubic_chebyshev, label='Cubic Spline Interpolation', color='orange')
-axs[1,0].set_title('Cubic Spline Interpolation (Chebyshev Nodes)')
+    axs[0,1].plot(x_interpolated_lagrange_chebyshev, lagrange_error_values_chebyshev, label='Lagrange Error', color='red')
+    axs[0,1].set_title('Lagrange Error (Chebyshev Nodes)')
 
-axs[1,1].plot(x_interpolated_cubic_chebyshev, cubic_spline_error_values_chebyshev, label='Cubic Spline Error', color='red')
-axs[1,1].set_title('Cubic Spline Error (Chebyshev Nodes)')
+    axs[1,0].plot(x_ground_truth, y_ground_truth, label='Ground Truth', linestyle='--', color='blue')
+    axs[1,0].scatter(x_values_chebyshev, y_values_chebyshev, label='Interpolation Points', color='red', s=8)
+    axs[1,0].plot(x_interpolated_cubic_chebyshev, y_interpolated_cubic_chebyshev, label='Cubic Spline Interpolation', color='orange')
+    axs[1,0].set_title('Cubic Spline Interpolation (Chebyshev Nodes)')
 
-plt.tight_layout()
-plt.show()
+    axs[1,1].plot(x_interpolated_cubic_chebyshev, cubic_spline_error_values_chebyshev, label='Cubic Spline Error', color='red')
+    axs[1,1].set_title('Cubic Spline Error (Chebyshev Nodes)')
+
+    plt.tight_layout()
+    plt.show()
+    return
+
+if(__name__ == "__main__"):
+    main()
---
+++ ej2.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+from scipy.optimize import minimize
+
+# Step 1: Read CSV file
+file_path = "mnyo_ground_truth.csv"
+data = pd.read_csv(file_path, header=None, delimiter=' ', names=['x', 'y'])
+
+file_path_mediciones = "mnyo_mediciones.csv"
+data_mediciones = pd.read_csv(file_path_mediciones, header=None, delimiter=' ', names=['x', 'y'])
+
+# Calculate cumulative distance as 't'
+data_mediciones['t'] = np.arange(1, len(data_mediciones) + 1)
+
+# Interpolate using cubic spline with respect to 't'
+cs_x = CubicSpline(data_mediciones['t'], data_mediciones['x'])
+cs_y = CubicSpline(data_mediciones['t'], data_mediciones['y'])
+
+# Generate new t-values for smoother plot
+t_new = np.linspace(data_mediciones['t'].min(), data_mediciones['t'].max(), 1000)
+
+# Obtain interpolated x and y values for the new t-values
+x_new = cs_x(t_new)
+y_new = cs_y(t_new)
+
+# Read CSV file
+file_path_mediciones2 = "mnyo_mediciones2.csv"
+data_mediciones2 = pd.read_csv(file_path_mediciones2, header=None, delimiter=' ', names=['x', 'y'])
+
+# Calculate cumulative distance as 't'
+data_mediciones2['t'] = np.arange(1, len(data_mediciones2) + 1)
+
+# Interpolate using cubic spline with respect to 't'
+cs_x2 = CubicSpline(data_mediciones2['t'], data_mediciones2['x'])
+cs_y2 = CubicSpline(data_mediciones2['t'], data_mediciones2['y'])
+
+# Generate new t-values for smoother plot
+t_new2 = np.linspace(data_mediciones2['t'].min(), data_mediciones2['t'].max(), 1000)
+
+# Obtain interpolated x and y values for the new t-values
+x_new2 = cs_x2(t_new2)
+y_new2 = cs_y2(t_new2)
+
+
+
+# Step 2: Plot the trajectory
+plt.figure(figsize=(8, 6))
+plt.plot(data['x'], data['y'], linestyle='-')
+plt.plot(x_new, y_new, 'r-', label='Cubic Splines Trajectory 1', linestyle='--')
+plt.plot(x_new2, y_new2, 'g-', label='Cubic Splines Trajectory 2', linestyle='--')
+plt.title('Trajectory Plot')
+plt.xlabel('X Coordinate')
+plt.ylabel('Y Coordinate')
+plt.legend()
+plt.grid(True)
+plt.show()

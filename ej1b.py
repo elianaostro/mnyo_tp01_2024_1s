6cf1390cd803ce077d67bd9f07f280be97e1546d
jojojab <jojojablon@gmail.com>
2024-03-28 12:32:03 -0300

    1b fachero

--- ej1b.py
+++ ej1b.py
@@ -43,50 +43,6 @@ Z_interp_linear = interp_func_linear(x_new, x_new)
 # Compute the absolute difference (error) between the original and interpolated values
 error_linear = np.abs(f(X1_new, X2_new) - Z_interp_linear)
 
-# plt.figure(figsize=(12, 6))
-
-# plt.subplot(1, 2, 1)
-# plt.imshow(error_cubic, extent=(-1, 1, -1, 1), cmap='plasma')
-# plt.colorbar(label='Absolute Error')
-# plt.title('Absolute Error (cubic interpolation)')
-# plt.xlabel('X1')
-# plt.ylabel('X2')
-
-# plt.subplot(1, 2, 2)
-# plt.imshow(error_linear, extent=(-1, 1, -1, 1), cmap='plasma')
-# plt.colorbar(label='Absolute Error')
-# plt.title('Absolute Error (linear interpolation)')
-# plt.xlabel('X1')
-# plt.ylabel('X2')
-
-# plt.tight_layout()
-# plt.show()
-
-
-# # Plot original function
-# fig = plt.figure(figsize=(12, 6))
-
-# ax1 = fig.add_subplot(1, 3, 1, projection='3d')
-# ax1.plot_surface(X1, X2, Z, cmap='viridis')
-# ax1.set_title('Original Function')
-
-
-# # Plot interpolated function using cubic interpolation
-# ax2 = fig.add_subplot(1, 3, 2, projection='3d')
-# ax2.plot_surface(X1_new, X2_new, Z_new, cmap='plasma')
-# ax2.plot_surface(X1, X2, Z, color=(0, 0, 1, 0.2))
-# ax2.set_title('Cubic Interpolation')
-
-
-# # Plot interpolated function using linear interpolation
-# ax3 = fig.add_subplot(1, 3, 3, projection='3d')
-# ax3.plot_surface(X1_new2, X2_new2, Z_new2, cmap='plasma')
-# ax3.plot_surface(X1, X2, Z, color=(0, 0, 1, 0.2))
-# ax3.set_title('Linear Interpolation')
-
-# plt.show()
-
-
 # Original function plot
 fig1 = plt.figure(figsize=(12, 9))
 ax_original = fig1.add_subplot(111, projection='3d')
@@ -128,6 +84,100 @@ plt.show()
 fig5 = plt.figure(figsize=(8, 6))
 ax_error_cubic = fig5.add_subplot(111)
 cubic_error_plot = ax_error_cubic.imshow(error_cubic, extent=(-1, 1, -1, 1), cmap='plasma')
+cubic_colorbar = plt.colorbar(cubic_error_plot, ax=ax_error_cubic)
+ax_error_cubic.set_title('Absolute Error (Cubic Interpolation)')
+
+# Set scientific notation for the colorbar ticks
+cubic_colorbar.formatter.set_powerlimits((-3, 3))
+cubic_colorbar.update_ticks()
+
+plt.show()
+
+# Generate Chebyshev nodes
+def chebyshev_nodes(n, a=-1, b=1):
+    """Generate Chebyshev nodes in the interval [a, b]."""
+    k = np.arange(1, n + 1)
+    nodes = 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) * np.pi / (2 * n))
+    return np.sort(nodes)
+
+# Number of Chebyshev nodes
+n_cheb = 20
+cheb_nodes_x1 = chebyshev_nodes(n_cheb)
+cheb_nodes_x2 = chebyshev_nodes(n_cheb)
+
+
+# Generate grid points
+x1_cheb = np.linspace(-1, 1, 50)
+x2_cheb = np.linspace(-1, 1, 50)
+X1_cheb, X2_cheb = np.meshgrid(x1_cheb, x2_cheb)
+Z_cheb = f(X1_cheb, X2_cheb)
+
+interp_func_cheb = interp2d(x1_cheb, x2_cheb, Z_cheb, kind='cubic')
+
+# New grid for interpolated values
+x_new_cheb = np.linspace(-1, 1, 15)
+X1_new_cheb, X2_new_cheb = np.meshgrid(x_new_cheb, x_new_cheb)
+Z_new_cheb = interp_func_cheb(x_new_cheb, x_new_cheb)
+
+# Interpolate the function using a different method
+interp_func2_cheb = interp2d(x1_cheb, x2_cheb, Z_cheb, kind='linear')
+# New grid for interpolated values
+x_new2_cheb = np.linspace(-1, 1, 15)
+X1_new2_cheb, X2_new2_cheb = np.meshgrid(x_new2_cheb, x_new2_cheb)
+Z_new2_cheb = interp_func2_cheb(x_new2_cheb, x_new2_cheb)
+
+# Compute the absolute difference (error) between the original and interpolated values
+error_cubic_cheb = np.abs(f(X1_new_cheb, X2_new_cheb) - Z_new_cheb)
+
+# Interpolate the function using 'linear' method
+interp_func_linear_cheb = interp2d(x1_cheb, x2_cheb, Z_cheb, kind='linear')
+Z_interp_linear_cheb = interp_func_linear_cheb(x_new_cheb, x_new_cheb)
+
+# Compute the absolute difference (error) between the original and interpolated values
+error_linear_cheb = np.abs(f(X1_new_cheb, X2_new_cheb) - Z_interp_linear_cheb)
+
+
+
+# Original function plot
+fig1 = plt.figure(figsize=(12, 9))
+ax_original = fig1.add_subplot(111, projection='3d')
+ax_original.plot_surface(X1_cheb, X2_cheb, Z_cheb, cmap='viridis')
+ax_original.set_title('Original Function Chebysheb')
+plt.show()
+
+# Linear interpolation plot
+fig2 = plt.figure(figsize=(12, 9))
+ax_interp_linear_cheb = fig2.add_subplot(111, projection='3d')
+ax_interp_linear_cheb.plot_surface(X1_new2_cheb, X2_new2_cheb, Z_new2_cheb, cmap='plasma')
+ax_interp_linear_cheb.set_title('Linear Interpolation Chebysheb')
+
+plt.show()
+
+# Linear error plot
+fig3 = plt.figure(figsize=(8, 6))
+ax_error_linear = fig3.add_subplot(111)
+linear_error_plot = ax_error_linear.imshow(error_linear_cheb, extent=(-1, 1, -1, 1), cmap='plasma')
+linear_colorbar = plt.colorbar(linear_error_plot, ax=ax_error_linear)
+ax_error_linear.set_title('Absolute Error (Linear Interpolation) Chebysheb')
+
+# Set scientific notation for the colorbar ticks
+linear_colorbar.formatter.set_powerlimits((-3, 3))
+linear_colorbar.update_ticks()
+
+plt.show()
+
+# Cubic interpolation plot
+fig4 = plt.figure(figsize=(12, 9))
+ax_interp_cubic = fig4.add_subplot(111, projection='3d')
+ax_interp_cubic.plot_surface(X1_new_cheb, X2_new_cheb, Z_new_cheb, cmap='plasma')
+ax_interp_cubic.set_title('Cubic Interpolation')
+
+plt.show()
+
+# Cubic error plot
+fig5 = plt.figure(figsize=(8, 6))
+ax_error_cubic = fig5.add_subplot(111)
+cubic_error_plot = ax_error_cubic.imshow(error_cubic_cheb, extent=(-1, 1, -1, 1), cmap='plasma')
 cubic_colorbar = plt.colorbar(cubic_error_plot, ax=ax_error_cubic)
 ax_error_cubic.set_title('Absolute Error (Cubic Interpolation)')
 
--- ej1b2.py
+++ ej1b2.py
@@ -1,67 +1,8 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# from scipy.interpolate import interp2d, CubicSpline
-
-# def f(x1, x2):
-#     return (
-#                 0.75 * np.exp ( - ( (10 * x1 - 2 ) ** 2 ) / 4 - ( ( 9 * x2 - 2 ) ** 2 ) / 4 ) + 
-#                 0.65 * np.exp ( - ( ( 9 * x1 + 1 ) ** 2 ) / 9 - ( (10 * x2 + 1 ) ** 2 ) / 2 ) + 
-#                 0.55 * np.exp ( - ( ( 9 * x1 - 6 ) ** 2 ) / 4 - ( ( 9 * x2 - 3 ) ** 2 ) / 4 ) - 
-#                 0.01 * np.exp ( - ( ( 9 * x1 - 7 ) ** 2 ) / 4 - ( ( 9 * x2 - 3 ) ** 2 ) / 4 )
-#             )
-
-# # Generate grid points
-# x1 = np.linspace(-1, 1, 50)
-# x2 = np.linspace(-1, 1, 50)
-# X1, X2 = np.meshgrid(x1, x2)
-# Z = f(X1, X2)
-
-# # Generate Chebyshev nodes
-# def chebyshev_nodes(n, a=-1, b=1):
-#     """Generate Chebyshev nodes in the interval [a, b]."""
-#     k = np.arange(1, n + 1)
-#     nodes = 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) * np.pi / (2 * n))
-#     return np.sort(nodes)
-
-# # Number of Chebyshev nodes
-# n_cheb = 20
-# cheb_nodes_x1 = chebyshev_nodes(n_cheb)
-# cheb_nodes_x2 = chebyshev_nodes(n_cheb)
-# cheb_nodes_X1, cheb_nodes_X2 = np.meshgrid(cheb_nodes_x1, cheb_nodes_x2)
-
-# # Interpolate using Chebyshev nodes
-# interp_func_cheb = interp2d(cheb_nodes_x1, cheb_nodes_x2, f(cheb_nodes_X1, cheb_nodes_X2), kind='cubic')
-# Z_interp_cheb = interp_func_cheb(x1, x2)
-
-# # Interpolate using linear method
-# interp_func_linear = interp2d(x1, x2, Z, kind='linear')
-# Z_interp_linear = interp_func_linear(x1, x2)
-
-# # Compute absolute errors
-# error_cheb = np.abs(f(X1, X2) - Z_interp_cheb)
-# error_linear = np.abs(f(X1, X2) - Z_interp_linear)
-
-# # Plot
-# fig = plt.figure(figsize=(16, 8))
-# gs = fig.add_gridspec(1, 2, width_ratios=[1, 1])
-
-# # Plot Chebyshev interpolation
-# ax1 = fig.add_subplot(gs[0, 0], projection='3d')
-# ax1.plot_surface(X1, X2, Z_interp_cheb, cmap='plasma')
-# ax1.set_title('Chebyshev Interpolation')
-
-# # Plot linear interpolation
-# ax2 = fig.add_subplot(gs[0, 1], projection='3d')
-# ax2.plot_surface(X1, X2, Z_interp_linear, cmap='plasma')
-# ax2.set_title('Linear Interpolation')
-
-# plt.tight_layout()
-# plt.show()
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import interp2d
+import matplotlib.gridspec as gridspec
 
 def f(x1, x2):
     return (
@@ -71,56 +12,90 @@ def f(x1, x2):
                 0.01 * np.exp ( - ( ( 9 * x1 - 7 ) ** 2 ) / 4 - ( ( 9 * x2 - 3 ) ** 2 ) / 4 )
             )
 
-# Generate grid points
-x1 = np.linspace(-1, 1, 50)
-x2 = np.linspace(-1, 1, 50)
-X1, X2 = np.meshgrid(x1, x2)
-Z = f(X1, X2)
+def generate_equiespaced_points(n, a=-1, b=1):
+    """Generate n equiespaced points in the interval [a, b]."""
+    x1 = np.linspace(a, b, n)
+    x2 = np.linspace(a, b, n)
+    X1, X2 = np.meshgrid(x1, x2)
+    Z = f(X1, X2)
+    return X1, X2, Z
 
-# Generate Chebyshev nodes
 def chebyshev_nodes(n, a=-1, b=1):
     """Generate Chebyshev nodes in the interval [a, b]."""
     k = np.arange(1, n + 1)
     nodes = 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) * np.pi / (2 * n))
     return np.sort(nodes)
 
-# Number of Chebyshev nodes
-n_cheb = 20
-cheb_nodes_x1 = chebyshev_nodes(n_cheb)
-cheb_nodes_x2 = chebyshev_nodes(n_cheb)
-cheb_nodes_X1, cheb_nodes_X2 = np.meshgrid(cheb_nodes_x1, cheb_nodes_x2)
+def generate_chebyshev_points(n):
+    x1 = chebyshev_nodes(n)
+    x2 = chebyshev_nodes(n)
+    X1, X2 = np.meshgrid(x1, x2)
+    Z = f(X1, X2)
+    return X1, X2, Z
 
-# Interpolate using Chebyshev nodes
-interp_func_cheb = interp2d(cheb_nodes_x1, cheb_nodes_x2, f(cheb_nodes_X1, cheb_nodes_X2), kind='cubic')
-Z_interp_cheb = interp_func_cheb(x1, x2)
+def spline_interpolation(X1, X2, Z, kind='cubic'):
+    interp_func = interp2d(X1[0], X2[:, 0], Z, kind=kind)  # Use 1-D arrays for X1 and X2
+    x_new = np.linspace(min(X1[0]), max(X1[0]), 15)
+    X1_new, X2_new = np.meshgrid(x_new, x_new)
+    Z_new = interp_func(x_new, x_new)
+    return X1_new, X2_new, Z_new
 
-# Interpolate using linear method
-interp_func_linear = interp2d(x1, x2, Z, kind='linear')
-Z_interp_linear = interp_func_linear(x1, x2)
+def error_interpolation(X1, X2, Z_new):
+    return np.abs(f(X1, X2) - Z_new)
 
-# Compute absolute errors
-error_cheb = np.abs(f(X1, X2) - Z_interp_cheb)
-error_linear = np.abs(f(X1, X2) - Z_interp_linear)
+def plot_original_function(X1, X2, Z):
+    fig1 = plt.figure(figsize=(12, 9))
+    ax_original = fig1.add_subplot(111, projection='3d')
+    ax_original.plot_surface(X1, X2, Z, cmap='viridis')
+    ax_original.set_title('Original Function')
+    plt.show()
 
-# Plot
-fig = plt.figure(figsize=(16, 8))
-gs = fig.add_gridspec(2, 4)
+def plot_interpolation(X1_new, X2_new, Z_new, kind):
+    fig2 = plt.figure(figsize=(12, 9))
+    ax_interp = fig2.add_subplot(111, projection='3d')
+    ax_interp.plot_surface(X1_new, X2_new, Z_new, cmap='plasma')
+    ax_interp.set_title(f'{kind} Interpolation')
+    plt.show()
 
-# Plot Chebyshev interpolation
-ax1 = fig.add_subplot(gs[0, 0], projection='3d')
-ax1.plot_surface(X1, X2, Z_interp_cheb, cmap='plasma')
-ax1.set_title('Chebyshev Interpolation')
+def plot_error(X1, X2, error, kind):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(error, extent=(min(X1[0]), max(X1[0]), min(X2[:, 0]), max(X2[:, 0])), cmap='plasma', origin='lower')
+    plt.colorbar(label='Absolute Error')
+    plt.title(f'{kind} Interpolation Error')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.show()
 
-# Plot linear interpolation
-ax2 = fig.add_subplot(gs[0, 1], projection='3d')
-ax2.plot_surface(X1, X2, Z_interp_linear, cmap='plasma')
-ax2.set_title('Linear Interpolation')
 
-# Plot Chebyshev error
-ax3 = fig.add_subplot(gs[1, 0])
-cheb_error_plot = ax3.imshow(error_cheb, extent=(-1, 1, -1, 1), cmap='plasma')
-ax3.set_title('Chebyshev Error')
-plt.colorbar(cheb_error_plot, ax=ax3)
+def main():
+    x1, x2, z = generate_equiespaced_points(50)
+    x1_chebyshev, x2_chebyshev, z_chebyshev = generate_chebyshev_points(50)
 
-plt.tight_layout()
-plt.show()
+    x1_cubic, x2_cubic, z_new_cubic = spline_interpolation(x1, x2, z, 'cubic')
+    error_cubic = error_interpolation(x1_cubic, x2_cubic, z_new_cubic)
+
+    x1_chebyshev_cubic, x2_chebyshev_cubic, z_chebyshev_cubic = spline_interpolation(x1_chebyshev, x2_chebyshev, z_chebyshev, 'cubic')
+    error_chebyshev = error_interpolation(x1_chebyshev_cubic, x2_chebyshev_cubic, z_chebyshev_cubic)
+
+    x1_linear, x2_linear, z_new_linear = spline_interpolation(x1, x2, z, 'linear')
+    error_linear = error_interpolation(x1_linear, x2_linear, z_new_linear)
+    
+    x1_chebyshev_linear, x2_chebyshev_linear, z_chebyshev_linear = spline_interpolation(x1_chebyshev, x2_chebyshev, z_chebyshev, 'linear')
+    error_chebyshev_linear = error_interpolation(x1_chebyshev_linear, x2_chebyshev_linear, z_chebyshev_linear)    
+
+    plot_original_function(x1, x2, z)
+    plot_interpolation(x1_cubic, x2_cubic, z_new_cubic, 'Cubic Spline')
+    plot_error(x1_cubic, x2_cubic, error_cubic, 'Cubic Spline')
+
+    plot_interpolation(x1_chebyshev_cubic, x2_chebyshev_cubic, z_chebyshev_cubic, 'Cubic Spline Chebyshev')
+    plot_error(x1_chebyshev_cubic, x2_chebyshev_cubic, error_chebyshev, 'Cubic Spline Chebyshev')
+
+    plot_interpolation(x1_linear, x2_linear, z_new_linear, 'Linear Spline')
+    plot_error(x1_linear, x2_linear, error_linear, 'Linear Spline')
+
+    plot_interpolation(x1_chebyshev_linear, x2_chebyshev_linear, z_chebyshev_linear, 'Linear Spline Chebyshev')
+    plot_error(x1_chebyshev_linear, x2_chebyshev_linear, error_chebyshev_linear, 'Linear Spline Chebyshev')
+
+
+if __name__ == '__main__':
+    main()
